7d9baf887416f11eb811f94339269e27a8d1e892
MominIshtiaq <momin989ishtiaq@gmail.com>
2024-12-03 19:33:01 +0500

    display chat messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,64 @@
-
+import { useState } from "react"
+import Chat from "./components/Chat/Chat"
+import styles from "./App.module.css"
 
 function App() {
+  const [messages, setMessages] = useState(MESSAGES)
+
   return (
-    <>AI CHATBOT</>
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        <img className={styles.Logo} src="/chat-bot.png" alt="chatbot_img" />
+        <h2 className={styles.Title}>AI Chatbot</h2>
+      </header>
+      <div className={styles.ChatContainer}>
+        <Chat messages={messages}/>
+      </div>
+    </div>
   )
 }
+
+const MESSAGES = [
+  {
+    'role': 'user',
+    'content': 'Hello'
+  },
+  {
+    'role': 'assistant',
+    'content': 'Hey! How can I help you today?'
+  },
+  {
+    'role': 'user',
+    'content': 'Hello'
+  },
+  {
+    'role': 'assistant',
+    'content': 'Hey! How can I help you today?'
+  },
+  {
+    'role': 'user',
+    'content': 'Hello'
+  },
+  {
+    'role': 'assistant',
+    'content': 'Hey! How can I help you today?'
+  },
+  {
+    'role': 'user',
+    'content': 'Hello'
+  },
+  {
+    'role': 'assistant',
+    'content': 'Hey! How can I help you today?'
+  },
+  {
+    'role': 'user',
+    'content': 'Hello'
+  },
+  {
+    'role': 'assistant',
+    'content': 'Hey! How can I help you today?'
+  },
+]
 
 export default App
