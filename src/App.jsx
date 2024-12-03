72acf07a18090e4b6206d4f8d688860b1398cebf
MominIshtiaq <momin989ishtiaq@gmail.com>
2024-12-03 20:02:31 +0500

    Handle Message input

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react"
 import Chat from "./components/Chat/Chat"
+import Controls from "./components/Controls/Controls"
 import styles from "./App.module.css"
 
 function App() {
-  const [messages, setMessages] = useState(MESSAGES)
+  const [messages, setMessages] = useState([])
+
+  function handleContentSend(content) {
+    setMessages((pervMessages) => [...pervMessages, { content, role: "user" }])
+  }
 
   return (
     <div className={styles.App}>
@@ -14,51 +19,9 @@ function App() {
       <div className={styles.ChatContainer}>
         <Chat messages={messages}/>
       </div>
+      <Controls onSend={handleContentSend} />
     </div>
   )
 }
-
-const MESSAGES = [
-  {
-    'role': 'user',
-    'content': 'Hello'
-  },
-  {
-    'role': 'assistant',
-    'content': 'Hey! How can I help you today?'
-  },
-  {
-    'role': 'user',
-    'content': 'Hello'
-  },
-  {
-    'role': 'assistant',
-    'content': 'Hey! How can I help you today?'
-  },
-  {
-    'role': 'user',
-    'content': 'Hello'
-  },
-  {
-    'role': 'assistant',
-    'content': 'Hey! How can I help you today?'
-  },
-  {
-    'role': 'user',
-    'content': 'Hello'
-  },
-  {
-    'role': 'assistant',
-    'content': 'Hey! How can I help you today?'
-  },
-  {
-    'role': 'user',
-    'content': 'Hello'
-  },
-  {
-    'role': 'assistant',
-    'content': 'Hey! How can I help you today?'
-  },
-]
 
 export default App
--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,8 +1,13 @@
 import styles from "./Chat.module.css"
 
+const WELCOME_MESSAGE = {
+  role: "assistant",
+  content: "Hello! How can i help you?"
+}
+
 const Chat = ({ messages }) => {
   return (
-    <div className={styles.Chat}>{messages.map(({role, content}, index) => (
+    <div className={styles.Chat}>{[WELCOME_MESSAGE, ...messages].map(({role, content}, index) => (
         <div key={index} className={styles.Message} data-role={role}>
             {content}
         </div>
