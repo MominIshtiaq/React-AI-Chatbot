da386c1c8cf3db4dcd2ed42b4640b5f65d0ada4b
MominIshtiaq <momin989ishtiaq@gmail.com>
2025-02-01 20:31:22 +0500

    Added Gemini

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import { useState } from "react"
-import Chat from "./components/Chat/Chat"
-import Controls from "./components/Controls/Controls"
-import styles from "./App.module.css"
+import { useState } from "react";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import Chat from "./components/Chat/Chat";
+import Controls from "./components/Controls/Controls";
+import styles from "./App.module.css";
+
+const google_ai = new GoogleGenerativeAI(
+  import.meta.env.VITE_GOOGLE_AI_API_KEY
+);
+const gemini = google_ai.getGenerativeModel({ model: "gemini-1.5-flash" });
+const chat = gemini.startChat({ history: [] });
+let result = await chat.sendMessage("I have 2 dogs in my house.");
+console.log(result.response.text());
 
 function App() {
-  const [messages, setMessages] = useState([])
+  const [messages, setMessages] = useState([]);
 
-  function handleContentSend(content) {
-    setMessages((pervMessages) => [...pervMessages, { content, role: "user" }])
+  function addMessage(message) {
+    setMessages((pervMessages) => [...pervMessages, message]);
+  }
+
+  async function handleContentSend(content) {
+    addMessage({ content, role: "user" });
+    try {
+      const result = await chat.sendMessage(content);
+      addMessage({ content: result.response.text(), role: "assistant" });
+    } catch (error) {
+      addMessage({
+        content: "Sorry, I couldn't process your request, Please try again!",
+        role: "system",
+      });
+      console.error(error);
+    }
   }
 
   return (
@@ -17,11 +40,11 @@ function App() {
         <h2 className={styles.Title}>AI Chatbot</h2>
       </header>
       <div className={styles.ChatContainer}>
-        <Chat messages={messages}/>
+        <Chat messages={messages} />
       </div>
       <Controls onSend={handleContentSend} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
