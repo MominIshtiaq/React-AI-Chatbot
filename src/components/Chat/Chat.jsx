cd52bc4bc798e2f5c08941999c5a4d53e72a6a35
MominIshtiaq <momin989ishtiaq@gmail.com>
2025-02-08 19:00:35 +0500

    add text markup, autosize textfield and input focus

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,18 +1,31 @@
-import styles from "./Chat.module.css"
+import styles from "./Chat.module.css";
+import Markdown from "react-markdown";
+import PropTypes from "prop-types";
 
 const WELCOME_MESSAGE = {
   role: "assistant",
-  content: "Hello! How can i help you?"
-}
+  content: "Hello! How can i help you?",
+};
 
 const Chat = ({ messages }) => {
   return (
-    <div className={styles.Chat}>{[WELCOME_MESSAGE, ...messages].map(({role, content}, index) => (
+    <div className={styles.Chat}>
+      {[WELCOME_MESSAGE, ...messages].map(({ role, content }, index) => (
         <div key={index} className={styles.Message} data-role={role}>
-            {content}
+          <Markdown>{content}</Markdown>
         </div>
-    ))}</div>
-  )
-}
+      ))}
+    </div>
+  );
+};
 
-export default Chat
+Chat.propTypes = {
+  messages: PropTypes.arrayOf(
+    PropTypes.shape({
+      role: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Chat;
--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,59 +1,79 @@
-import { useState } from "react"
-import styles from "./Controls.module.css"
+import { useRef, useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import styles from "./Controls.module.css";
+import TextareaAutosize from "react-textarea-autosize";
 
-const Controls = ({ onSend }) => {
-  const [content, setContent] = useState("")
+const Controls = ({ isDisabled = false, onSend }) => {
+  const textareaRef = useRef(null);
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    if (!isDisabled) {
+      textareaRef.current.focus();
+    }
+  }, [isDisabled]);
 
   function handleContentChange(event) {
-    setContent(event.target.value)
+    setContent(event.target.value);
   }
 
-  function handleContentSend(event) {
+  function handleContentSend() {
     if (content.length > 0) {
-      onSend(content)
-      setContent("")
+      onSend(content);
+      setContent("");
     }
   }
 
   function handleEnterPress(event) {
-    if(event.key === "Enter" && !event.shiftKey) {
-      event.preventDefault()
-      handleContentSend()
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      handleContentSend();
     }
   }
 
   return (
     <div className={styles.Controls}>
-        <div className={styles.TextAreaContainer}>
-            <textarea 
-                className={styles.TextArea} 
-                placeholder="Message AI Chatbot" 
-                value={content}
-                onChange={handleContentChange}
-                onKeyDown={handleEnterPress}
-            />
-        </div>
-        <button className={styles.Button} onClick={handleContentSend}>
-            <SendIcon />
-        </button>
+      <div className={styles.TextAreaContainer}>
+        <TextareaAutosize
+          ref={textareaRef}
+          minRows={1}
+          maxRows={4}
+          disabled={isDisabled}
+          className={styles.TextArea}
+          placeholder="Message AI Chatbot"
+          value={content}
+          onChange={handleContentChange}
+          onKeyDown={handleEnterPress}
+        />
+      </div>
+      <button
+        className={styles.Button}
+        disabled={isDisabled}
+        onClick={handleContentSend}
+      >
+        <SendIcon />
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Controls
+Controls.propTypes = {
+  isDisabled: PropTypes.bool,
+  onSend: PropTypes.func.isRequired,
+};
 
-
+export default Controls;
 
 export const SendIcon = () => {
   return (
-    <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        height="24px" 
-        viewBox="0 -960 960 960" 
-        width="24px" 
-        fill="#5f6368"
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24px"
+      viewBox="0 -960 960 960"
+      width="24px"
+      fill="#5f6368"
     >
-        <path d="M120-160v-240l320-80-320-80v-240l760 320-760 320Z"/>
+      <path d="M120-160v-240l320-80-320-80v-240l760 320-760 320Z" />
     </svg>
-  )
-}
+  );
+};
