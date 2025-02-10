be53850754aab313167b4b053321b8e1975571fd
MominIshtiaq <momin989ishtiaq@gmail.com>
2025-02-10 11:35:33 +0500

    add dark theme using light-dark() css method

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,20 +1,45 @@
-import styles from "./Chat.module.css";
-import Markdown from "react-markdown";
+import { useEffect, useMemo, useRef } from "react";
 import PropTypes from "prop-types";
+import Markdown from "react-markdown";
+import styles from "./Chat.module.css";
 
-const WELCOME_MESSAGE = {
-  role: "assistant",
-  content: "Hello! How can i help you?",
-};
+const WELCOME_MESSAGE_GROUP = [
+  {
+    role: "assistant",
+    content: "Hello! How can i help you?",
+  },
+];
 
 const Chat = ({ messages }) => {
+  const messagesEndRef = useRef(null);
+  const messagesGroups = useMemo(
+    () =>
+      messages.reduce((groups, message) => {
+        if (message.role === "user") groups.push([]);
+        groups[groups.length - 1].push(message);
+        return groups;
+      }, []),
+    [messages]
+  );
+
+  useEffect(
+    () => messagesEndRef.current?.scrollIntoView({ behavior: "smooth" }),
+    [messages]
+  );
   return (
     <div className={styles.Chat}>
-      {[WELCOME_MESSAGE, ...messages].map(({ role, content }, index) => (
-        <div key={index} className={styles.Message} data-role={role}>
-          <Markdown>{content}</Markdown>
+      {[WELCOME_MESSAGE_GROUP, ...messagesGroups].map((group, index) => (
+        //Group
+        <div className={styles.Group} key={index}>
+          {group.map(({ role, content }, index) => (
+            //Message
+            <div key={index} className={styles.Message} data-role={role}>
+              <Markdown>{content}</Markdown>
+            </div>
+          ))}
         </div>
       ))}
+      <div ref={messagesEndRef} />
     </div>
   );
 };
